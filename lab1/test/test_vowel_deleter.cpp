12d55ca0b5aacf5106b4a6e9891b8356284aed00
Aartem <artemsorokin0301@gmail.com>
2023-09-18 22:13:17 +0300

    lab1

--- lab1/test/test_vowel_deleter.cpp
+++ lab1/test/test_vowel_deleter.cpp
@@ -3,43 +3,72 @@
 
 
 TEST(test_01, basic_test_set) {
-    ASSERT_EQ(vowel_deleter("Loser"), "Lsr");
+    std::string text = "Loser";
+    std::string pattern = vowel_deleter(text);
+    EXPECT_EQ(pattern, "Lsr");
 }
+
 
 TEST(test_02, basic_test_set) {
-    ASSERT_EQ(vowel_deleter("You are stupid!!!"), " r stpd!!!");
+    std::string text = "You are stupid!!!";
+    std::string pattern = vowel_deleter(text);
+    EXPECT_EQ(pattern, " r stpd!!!");
 }
+
 
 TEST(test_03, basic_test_set) {
-    ASSERT_EQ(vowel_deleter("GAAAA"), "G");
+    std::string text = "GAAAA";
+    std::string pattern = vowel_deleter(text);
+    EXPECT_EQ(pattern, "G");
 }
+
 
 TEST(test_04, basic_test_set) {
-    ASSERT_EQ(vowel_deleter("thank u"), "thnk ");
+    std::string text = "thank u";
+    std::string pattern = vowel_deleter(text);
+    EXPECT_EQ(pattern, "thnk ");
 }
+
 
 TEST(test_05, basic_test_set) {
-    ASSERT_EQ(vowel_deleter("This website is for losers LOL!"), "Ths wbst s fr lsrs LL!");
+    std::string text = "This website is for losers LOL!";
+    std::string pattern = vowel_deleter(text);
+    EXPECT_EQ(pattern, "Ths wbst s fr lsrs LL!");
 }
+
 
 TEST(test_06, basic_test_set) {
-    ASSERT_EQ(vowel_deleter("AAAAAAaaaaaaaAAAA"), "");
+    std::string text = "AAAAAAaaaaaaaAAAA";
+    std::string pattern = vowel_deleter(text);
+    EXPECT_EQ(pattern, "");
 }
+
 
 TEST(test_07, basic_test_set) {
-    ASSERT_EQ(vowel_deleter("Only TEST&"), "nl TST&");
+    std::string text = "Only TEST&";
+    std::string pattern = vowel_deleter(text);
+    EXPECT_EQ(pattern, "nl TST&");
 }
+
 
 TEST(test_08, basic_test_set) {
-    ASSERT_EQ(vowel_deleter("123"), "123");
+    std::string text = "123";
+    std::string pattern = vowel_deleter(text);
+    EXPECT_EQ(pattern, "123");
 }
+
 
 TEST(test_09, basic_test_set) {
-    ASSERT_EQ(vowel_deleter("Hello, World!"), "Hll, Wrld!");
+    std::string text = "Hello, World!";
+    std::string pattern = vowel_deleter(text);
+    EXPECT_EQ(pattern, "Hll, Wrld!");
 }
 
+
 TEST(test_10, basic_test_set) {
-    ASSERT_EQ(vowel_deleter("come on"), "cm n");
+    std::string text = "come on";
+    std::string pattern = vowel_deleter(text);
+    EXPECT_EQ(pattern, "cm n");
 }
 
 
