2105cd0f5f55f73e2d855e7b6c47a141568c22bb
Aartem <artemsorokin0301@gmail.com>
2023-10-06 21:51:34 +0300

    lab2

--- lab2/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/artemsorokin/Desktop/Programming/labs/lab2
-# Build directory: /Users/artemsorokin/Desktop/Programming/labs/lab2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[LAB1]=] "/Users/artemsorokin/Desktop/Programming/labs/lab2/build/tests2")
-set_tests_properties([=[LAB1]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/artemsorokin/Desktop/Programming/labs/lab2/CMakeLists.txt;17;add_test;/Users/artemsorokin/Desktop/Programming/labs/lab2/CMakeLists.txt;0;")
