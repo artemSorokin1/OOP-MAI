a85f4c86384a53eaaab6d3d59a34f0dcfb62118f
Aartem <artemsorokin0301@gmail.com>
2023-10-22 17:05:32 +0300

    lab3

--- lab3/build/CMakeFiles/lab3.dir/src/figures.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/lab3.dir/src/figures.cpp.o: \
-  /Users/artemsorokin/Desktop/Programming/labs/lab3/src/figures.cpp
--- lab3/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/artemsorokin/Desktop/Programming/labs/lab3
-# Build directory: /Users/artemsorokin/Desktop/Programming/labs/lab3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[LAB1]=] "/Users/artemsorokin/Desktop/Programming/labs/lab3/build/tests3")
-set_tests_properties([=[LAB1]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/artemsorokin/Desktop/Programming/labs/lab3/CMakeLists.txt;17;add_test;/Users/artemsorokin/Desktop/Programming/labs/lab3/CMakeLists.txt;0;")
